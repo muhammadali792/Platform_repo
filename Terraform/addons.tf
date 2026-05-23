d6915fb0e591cae3cdb44ef98c45fe4052c3e394
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-23 17:30:51 +0500

    Update addons.tf

--- Terraform/addons.tf
+++ Terraform/addons.tf
@@ -1,6 +1,6 @@
 module "eks_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0"
+  version = "~> 1.16"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
