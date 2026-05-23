a59e71dd9865cab11879f7731d2db7baad9bbba5
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-23 20:36:23 +0500

    Update iam.tf

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,34 +1,31 @@
-# 1. Yeh block variables par loop chala kar har addon ke liye alag se IAM Role banata hai aur naye module ki wajah se automatically un par EKS Pod Identity ki safe trust policy lagata hai.
-module "eks_pod_identity_roles" {
-  source   = "terraform-aws-modules/iam/aws//modules/iam-eks-role"
-  version  = "~> 5.0"
+resource "aws_iam_role" "addon_roles" {
   for_each = var.eks_addons_security_config
 
-  role_name = "${module.eks.cluster_name}-${each.key}-pod-identity"
+  name = "${module.eks.cluster_name}-${each.key}-pod-identity"
 
-  role_policy_arns = {
-    addon_policy = each.value.policy_arn
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "pods.eks.amazonaws.com" }
+      Action    = ["sts:AssumeRole", "sts:TagSession"]
+    }]
+  })
 
   tags = local.common_tags
 }
 
-# 2. Yeh block unhi bane huay AWS IAM Roles ko aapke Kubernetes cluster ke Service Accounts ke sath aapas mein jod (bind) deta hai taake pods ko permissions mil sakein.
-resource "aws_eks_pod_identity_association" "addon_associations" {
-  for_each = var.eks_addons_security_config
+resource "aws_iam_role_policy_attachment" "addon_roles" {
+  for_each   = var.eks_addons_security_config
+  role       = aws_iam_role.addon_roles[each.key].name
+  policy_arn = each.value.policy_arn
+}
 
+resource "aws_eks_pod_identity_association" "addon_associations" {
+  for_each        = var.eks_addons_security_config
   cluster_name    = module.eks.cluster_name
   namespace       = each.value.namespace
   service_account = each.value.service_account
-  role_arn        = module.eks_pod_identity_roles[each.key].iam_role_arn
-}
+  role_arn        = aws_iam_role.addon_roles[each.key].arn
 
-# 3. Yeh variables ki declaration hai jo batati hai ke input data ka structure kaisa hoga.
-variable "eks_addons_security_config" {
-  description = "Map of EKS addons configuration for dynamic Pod Identity IAM roles execution"
-  type = map(object({
-    namespace       = string
-    service_account = string
-    policy_arn      = string
-  }))
-}
+ 
