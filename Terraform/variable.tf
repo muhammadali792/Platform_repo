a861ef7d3a98e9840e663e69423138d49be3b7a0
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-23 16:41:50 +0500

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -27,3 +27,7 @@ variable "availability_zones" {
   type    = list(string)
   default = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
 }
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
