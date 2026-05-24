2faa2744f85df396bfbbc40cea620f1e8008710c
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-24 10:33:05 +0500

    Update karpenter.tf

--- Terraform/karpenter.tf
+++ Terraform/karpenter.tf
@@ -1,21 +1,17 @@
 resource "helm_release" "karpenter" {
   namespace        = "karpenter"
   create_namespace = true
-
   name       = "karpenter"
   repository = "oci://public.ecr.aws/karpenter"
   chart      = "karpenter"
   version    = "1.0.6"
-
-  # Industry Standard Template Loading
   values = [
     templatefile("${path.module}/templates/karpenter-values.yaml", {
-      cluster_name     = "Karpenter-${var.cluster_name}"
+      cluster_name     = var.cluster_name
       cluster_endpoint = module.eks.cluster_endpoint
       iam_role_arn     = module.karpenter_iam.iam_role_arn
     })
   ]
-
   depends_on = [module.eks, module.karpenter_iam]
 }
 
@@ -25,13 +21,12 @@ resource "kubectl_manifest" "node_class" {
     node_iam_role_name = module.karpenter_iam.node_iam_role_name
     environment        = var.environment
     cluster_version    = var.cluster_version
+    cluster_dns        = cidrhost(module.eks.cluster_service_cidr, 10)
   })
-
   depends_on = [helm_release.karpenter]
 }
 
 resource "kubectl_manifest" "node_pool_general" {
   yaml_body = templatefile("${path.module}/templates/node-pool.yml", {})
-
   depends_on = [kubectl_manifest.node_class]
 }
