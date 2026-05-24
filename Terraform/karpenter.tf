9abd52ee69309aa94bce3ccae6dcaa6a30509a02
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-24 10:57:17 +0500

    Update karpenter.tf

--- Terraform/karpenter.tf
+++ Terraform/karpenter.tf
@@ -7,7 +7,7 @@ resource "helm_release" "karpenter" {
   version    = "1.0.6"
   values = [
     templatefile("${path.module}/templates/karpenter-values.yaml", {
-      cluster_name     = var.cluster_name
+      cluster_name     = Karpenter-${var.cluster_name}
       cluster_endpoint = module.eks.cluster_endpoint
       iam_role_arn     = module.karpenter_iam.iam_role_arn
     })
