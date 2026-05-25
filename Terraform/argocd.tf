31962e8fe96cbaa5976afbf6ddf9a9d3d46ae677
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-25 20:53:02 +0500

    Update argocd.tf

--- Terraform/argocd.tf
+++ Terraform/argocd.tf
@@ -1,20 +1,31 @@
 locals {
   app_values = {
-    # ArgoCD ke liye specific component keys
     argocd = {
       server       = { nodeSelector = { "role" = "system" }, tolerations = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }] }
       controller   = { nodeSelector = { "role" = "system" }, tolerations = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }] }
       repoServer   = { nodeSelector = { "role" = "system" }, tolerations = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }] }
       applicationSet = { nodeSelector = { "role" = "system" }, tolerations = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }] }
     }
-    # Rollouts ke liye
     argo_rollouts = {
       controller = { nodeSelector = { "role" = "system" }, tolerations = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }] }
+    }
+    # Yahan "prometheus" key add kar di gayi hai
+    prometheus = {
+      prometheus = {
+        prometheusSpec = {
+          nodeSelector = { "role" = "system" }
+          tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
+        }
+      }
+      grafana = {
+        nodeSelector = { "role" = "system" }
+        tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
+      }
     }
   }
 }
 
-# Helm Resources
+# Helm Resources (Ab ye kaam karega)
 resource "helm_release" "argo_cd" {
   name       = "argo-cd"
   chart      = "argo-cd"
@@ -38,5 +49,5 @@ resource "helm_release" "kube_prometheus_stack" {
   repository = "https://prometheus-community.github.io/helm-charts"
   namespace  = "kube-prometheus-stack"
   create_namespace = true
-  values     = [yamlencode(local.app_values.prometheus)]
+  values     = [yamlencode(local.app_values.prometheus)] # Ab ye match kar raha hai
 }
