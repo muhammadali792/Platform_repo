e427a27c95d2168892132a13ef1c8513eabe6dd2
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-25 16:39:04 +0500

    Update addons.tf

--- Terraform/addons.tf
+++ Terraform/addons.tf
@@ -1,6 +1,7 @@
 module "eks_addons" {
+  # Version 2.x update kiya gaya hai
   source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
@@ -9,66 +10,33 @@ module "eks_addons" {
 
   # AWS LOAD BALANCER CONTROLLER
   enable_aws_load_balancer_controller = true
-  aws_load_balancer_controller = {
-    values = [yamlencode({
-      tolerations = [{
-        key      = "CriticalAddonsOnly"
-        operator = "Equal"
-        value    = "true"
-        effect   = "NoSchedule"
-      }]
-      nodeSelector = { role = "system" }
-    })]
-  }
 
   # NGINX INGRESS
   enable_ingress_nginx = true
   ingress_nginx = {
     values = [yamlencode({
       controller = {
-        replicaCount = 2
-        tolerations = [{
-          key      = "CriticalAddonsOnly"
-          operator = "Equal"
-          value    = "true"
-          effect   = "NoSchedule"
-        }]
-        nodeSelector = { role = "system" }
-        config = {
-          "force-ssl-redirect" = "false"
-          "ssl-redirect"       = "false"
-        }
         service = {
           type = "LoadBalancer"
           annotations = {
-            "service.beta.kubernetes.io/aws-load-balancer-type" = "external"
+            "service.beta.kubernetes.io/aws-load-balancer-type"                    = "external"
             "service.beta.kubernetes.io/aws-load-balancer-target-group-attributes" = "deregistration_delay.timeout_seconds=30"
-            "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
-            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-protocol" = "HTTP"
-            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-port"     = "healthcheck" # Nginx controller ka internal port
-            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-path"     = "/healthz"
-            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-interval" = "10"
+            "service.beta.kubernetes.io/aws-load-balancer-scheme"                  = "internet-facing"
+            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-protocol"    = "HTTP"
+            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-port"        = "80"
+            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-path"        = "/healthz"
+            "service.beta.kubernetes.io/aws-load-balancer-healthcheck-interval"    = "10"
           }
-        }
-      }
-      admissionWebhooks = {
-        patch = {
-          tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
-          nodeSelector = { role = "system" }
-        }
-        createSecretJob = {
-          tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
-          nodeSelector = { role = "system" }
-        }
-        patchWebhookJob = {
-          tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
-          nodeSelector = { role = "system" }
         }
       }
     })]
   }
-  # ArgoCD Addon
+
+  # ARGO CD
   enable_argocd = true
+
+  # KUBE PROMETHEUS STACK
+  enable_kube_prometheus_stack = true
 
   # CORE ADDONS
   enable_metrics_server   = true
