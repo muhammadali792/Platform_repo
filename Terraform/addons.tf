059986b833f721ab6273697a0ecce3e172431748
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-05-25 16:50:40 +0500

    Update addons.tf

--- Terraform/addons.tf
+++ Terraform/addons.tf
@@ -7,42 +7,39 @@ module "eks_addons" {
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
 
-  # 1. AWS LOAD BALANCER CONTROLLER (Tolerations included)
+  # 1. AWS LOAD BALANCER CONTROLLER
   enable_aws_load_balancer_controller = true
-  aws_load_balancer_controller_helm_config = {
+  aws_load_balancer_controller = {
     values = [yamlencode({
       tolerations = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
       nodeSelector = { role = "system" }
-      webhook = {
-        tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
-        nodeSelector = { role = "system" }
-      }
     })]
   }
 
-  # 2. NGINX INGRESS (Tolerations & Annotations included)
+  # 2. NGINX INGRESS
   enable_ingress_nginx = true
-  ingress_nginx_helm_config = {
+  ingress_nginx = {
     values = [yamlencode({
       controller = {
         replicaCount = 2
         tolerations  = [{ key = "CriticalAddonsOnly", operator = "Equal", value = "true", effect = "NoSchedule" }]
         nodeSelector = { role = "system" }
-        config = {
-          "force-ssl-redirect" = "false"
-          "ssl-redirect"       = "false"
-        }
         service = {
           type = "LoadBalancer"
           annotations = {
             "service.beta.kubernetes.io/aws-load-balancer-type"                    = "external"
+
             "service.beta.kubernetes.io/aws-load-balancer-target-group-attributes" = "deregistration_delay.timeout_seconds=30"
+
             "service.beta.kubernetes.io/aws-load-balancer-scheme"                  = "internet-facing"
+
             "service.beta.kubernetes.io/aws-load-balancer-healthcheck-protocol"    = "HTTP"
+
             "service.beta.kubernetes.io/aws-load-balancer-healthcheck-port"        = "80"
+
             "service.beta.kubernetes.io/aws-load-balancer-healthcheck-path"        = "/healthz"
+
             "service.beta.kubernetes.io/aws-load-balancer-healthcheck-interval"    = "10"
-          }
         }
       }
     })]
@@ -50,31 +47,20 @@ module "eks_addons" {
 
   # 3. ARGO CD
   enable_argocd = true
-  argocd_helm_config = {
+  argocd = {
     values = [yamlencode({
-      server = {
-        service = { type = "ClusterIP" }
-      }
+      server = { service = { type = "ClusterIP" } }
     })]
   }
 
-  # 4. KUBE PROMETHEUS STACK
+  # 4. PROMETHEUS STACK
   enable_kube_prometheus_stack = true
-  kube_prometheus_stack_helm_config = {
+  kube_prometheus_stack = {
     values = [yamlencode({
-      prometheus = {
-        prometheusSpec = { retention = "5d" }
-      }
-      grafana = {
-        adminPassword = "admin"
-        service = { type = "ClusterIP" }
-      }
+      grafana = { adminPassword = "admin" }
     })]
   }
 
-  # 5. CORE ADDONS
   enable_metrics_server   = true
   enable_external_secrets = true
-
-  tags = local.common_tags
 }
