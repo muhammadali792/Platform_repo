7eb3443fe38aef3b2785260caaad984e169aa236
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-02 20:05:23 +0500

    Update secrets.tf

--- Terraform/secrets.tf
+++ Terraform/secrets.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
-    random = { source = "hashicorp/random", version = "3.6.0" }
-  }
-}
-
-provider "aws" { region = "us-east-1" }
-
 locals {
   services = {
     auth    = { rds = true,  redis = false, needs_jwt = true }
