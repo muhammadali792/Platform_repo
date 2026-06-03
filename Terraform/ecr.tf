27eb7d0704c8058cbfa39d093c8814768f37177f
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-03 09:01:11 +0500

    Refactor ECR repository configuration
    Removed lifecycle policy and output for ECR repositories.

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -1,14 +1,10 @@
 locals {
-  # Sirf service names ki list
   service_names = ["auth", "product", "order", "notif"]
 }
 
-# ECR Repositories
 resource "aws_ecr_repository" "service_repos" {
-  # List ko set mein convert kiya taake for_each kaam kare
   for_each = toset(local.service_names)
 
-  # Har item ke saath '-repo' jod diya
   name                 = "${each.value}-repo"
   image_tag_mutability = "MUTABLE"
 
@@ -17,22 +13,6 @@ resource "aws_ecr_repository" "service_repos" {
   }
 }
 
-# Lifecycle policy (Keep last 5 images)
-resource "aws_ecr_lifecycle_policy" "repo_policy" {
-  for_each   = aws_ecr_repository.service_repos
-  repository = each.value.name
-  
-  policy = jsonencode({
-    rules = [{
-      rulePriority = 1
-      description  = "Keep last 5 images"
-      selection    = { tagStatus = "any", countType = "imageCountMoreThan", countNumber = 5 }
-      action       = { type = "expire" }
-    }]
-  })
-}
-
-# Output
 output "ecr_repository_urls" {
   value = { for k, repo in aws_ecr_repository.service_repos : k => repo.repository_url }
 }
