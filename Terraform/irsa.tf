95056a88c12b0142080d4e959bc010d62f7cc15d
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-05 12:00:04 +0500

    Update irsa.tf

--- Terraform/irsa.tf
+++ Terraform/irsa.tf
@@ -1,18 +1,1 @@
-# 1. EBS CSI Driver ke liye IRSA (IAM Role) banayein
-module "ebs_csi_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.30"
 
-  role_name = "ebs-csi-controller-role"
-
-  role_policy_arns = {
-    policy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  }
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-}
