14e625bfa779f68224ae3c7228cab6dc7fa7ac39
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-12 15:13:45 +0500

    Update helm.tf

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -97,7 +97,8 @@ resource "helm_release" "argocd_image_updater" {
         name       = "ECR"
         api_url    = "https://${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
         prefix      = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
-        credsexpire = 0
+        credentials = "ext:/scripts/ecr-login.sh"
+        credsexpire = "12h"
       }]
       gitCredentials = [{
         url        = "https://github.com/muhammadali792"
