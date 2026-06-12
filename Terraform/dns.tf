c118c2ee5855aa4403ea6aca1adee4784f060587
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-12 10:43:28 +0500

    Update dns.tf

--- Terraform/dns.tf
+++ Terraform/dns.tf
@@ -1,8 +1,9 @@
+/*
 resource "aws_route53_zone" "main" {
   name = var.domain_name
   tags = local.common_tags
 }
-
+*/
 output "name_servers" {
   description = "Hostinger mein ye 4 NS records add karo"
   value       = aws_route53_zone.main.name_servers
