db356ce02f3e816b6fc3d0acd55701aebd6af5ea
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-12 10:41:34 +0500

    Remove commented-out Route 53 zone resource

--- Terraform/dns.tf
+++ Terraform/dns.tf
@@ -1,4 +1,3 @@
-/*
 resource "aws_route53_zone" "main" {
   name = var.domain_name
   tags = local.common_tags
@@ -8,4 +7,3 @@ output "name_servers" {
   description = "Hostinger mein ye 4 NS records add karo"
   value       = aws_route53_zone.main.name_servers
 }
-*/
