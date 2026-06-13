7591a4e9bed17f92e14f528d050bc271c4f28183
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-13 14:18:17 +0500

    Update route53.tf

--- Terraform/route53.tf
+++ Terraform/route53.tf
@@ -17,9 +17,6 @@ data "aws_lb" "nginx" {
 resource "aws_route53_zone" "main" {
   name = var.domain_name
   tags = local.common_tags
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_route53_record" "main" {
